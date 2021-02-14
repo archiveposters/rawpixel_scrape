cdb887ba702659843aa404899c6fc844824a2661
Ouanis Seddaoui <ouanis.seddaoui@smartsheet.com>
2021-02-14 23:19:36 +0000

    script to run in docker

--- README.md
+++ README.md
@@ -4,21 +4,21 @@ The project is aimed at returning the image titles and urls from rawpixel search
 
 ### Setup
 
-This script is to be used locally and will not work if run through GitPod
+This script runs on Docker. Make sure you have Docker installed in your environment.
 
-Version originally used Pycharm on Windows, however if system requirements are different please adjust accoringly.
 
-Clone the repo using:
+#### Build the image
 
-$ gh repo clone jamiebird119/web_scrape
+```
+docker build -t rawpixel_scrape
+```
 
-Install dependencies using pip. In the case of pycharm CLI command is as follows but depends on it being windows and on pycharm IDE.
+#### Run the container
 
-$ py -m pip install -r requirements.txt
+```
+docker run -v `pwd`/data:/app/data rawpixel_scrape
+```
 
-Ensure that the correct version of chromedriver is used for the corresponding version of chrome on windows. To check version click 3 dots in top right 
-go to Help > About Google Chrome. It will show you the version and then go to https://chromedriver.chromium.org/downloads and get the version for your operating system.
-Unzip file and place in project directory ensuring Path in run.py has been updated to your version (Path can easily be found by right clicking file in Pycharm and clicking copy path )
 
 Any urls for searches from rawpixel.com can then be added to urls.csv on a new row for each. 
 
--- run.py
+++ run.py
@@ -1,10 +1,23 @@
 from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
 import csv
 
 
+def set_chrome_options():
+    """Sets chrome options for Selenium.
+    Chrome options for headless browser is enabled.
+    """
+    chrome_options = Options()
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--no-sandbox")
+    chrome_options.add_argument("--disable-dev-shm-usage")
+    chrome_prefs = {}
+    chrome_options.experimental_options["prefs"] = chrome_prefs
+    chrome_prefs["profile.default_content_settings"] = {"images": 2}
+    return chrome_options
+
 def scrape(url):
-    # webdriver must match current version of chrome on correct OS from https://chromedriver.chromium.org/downloads and make sure the correct path is used to the version on user machine
-    driver = webdriver.Chrome(r"C:\Users\J\Documents\GitHub\selenium_base\chromedriver.exe")
+    driver = webdriver.Chrome(options=set_chrome_options())
     # load page
     driver.get(url)
 
@@ -34,7 +47,7 @@ def scrape(url):
 
     driver.close()
     # append to info.csv
-    with open("info.csv", mode="a") as fd:
+    with open("./data/info.csv", mode="a") as fd:
         fieldnames = ["title", "url"]
         writer = csv.DictWriter(fd, fieldnames=fieldnames)
         writer.writeheader()
@@ -42,14 +55,14 @@ def scrape(url):
             writer.writerow({"title": item[0], "url": item[1]})
 
 
-with open(r'C:\Users\J\Documents\GitHub\selenium_base\urls.csv', mode='r') as csv_file:
+with open('./data/urls.csv', mode='r') as csv_file:
     # reads csv file for urls in first column
     csv_reader = csv.DictReader(csv_file)
     row_count = 0
     urls = []
     for row in csv_reader:
         # writes url to new line of info.csv
-        with open('info.csv', mode='a') as new_file:
+        with open('./data/info.csv', mode='a') as new_file:
             fieldnames = ["url"]
             writer = csv.DictWriter(new_file,fieldnames=fieldnames)
             writer.writerow({"url": row['url']})
